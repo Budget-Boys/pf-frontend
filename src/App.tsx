51727180cc80e1751b3f6268cf2a40732dc53b94
EdOc-PS <eeuardooctavio@gmail.com>
2025-05-06 12:25:30 -0300

    feat: initial structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.sass";
+import test from './assets/MoodBoard.png';
+
+function App() {
+  return (
+    <>
+      <h1>Test</h1>
+      <img src={test} alt="" />
+    </>
+  );
+}
+
+export default App;
