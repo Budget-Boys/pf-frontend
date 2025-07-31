37a002067e3a492f9d3c54534812cfc8f22723b6
Eduardo Octávio <eeuardooctavio@gmail.com>
2025-07-31 08:45:19 -0300

    feat: add integration cards

--- src/features/main/dashboard/financialEntry/drawerContent/drawerContent.tsx
+++ src/features/main/dashboard/financialEntry/drawerContent/drawerContent.tsx
@@ -1,3 +1,4 @@
+import { useState, type ChangeEvent } from "react";
 import Input from "../../../../../components/input/input";
 import GroupExpanses from "../groups/groupExpanse";
 import GroupIncome from "../groups/groupIncome";
@@ -8,31 +9,54 @@ interface FinancialProps {
 }
 
 const DrawerFinancialData: React.FC<FinancialProps> = ({ type }) => {
+  const [form, setForm] = useState({
+    price: "",
+    radio: "",
+  });
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>): void {
+    const { id, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  }
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(form);
+  }
+
   return (
     <>
       <div className={style.container_financialData}>
         <div className={style.title_drawer}>
           <h1>Quanto você deseja adicionar?</h1>
         </div>
-        <form className={style.form_financialData}>
-            <Input placeholder={"R$: 100,00"} icon={"money"} type={"number"} />
+        <form className={style.form_financialData} onSubmit={handleSubmit}>
+          <Input
+            placeholder={"R$: 100,00"}
+            icon={"money"}
+            type={"number"}
+            onChange={handleChange}
+            inputName={"name"}
+          />
 
-            <div className={style.lineTitle}>
-              <h4>Fonte: </h4>
-            </div>
-            {type === "Renda" ? (
-              <GroupIncome
-                onChange={function (): void {
-                  throw new Error("Function not implemented.");
-                }}
-              />
-            ) : (
-              <GroupExpanses
-                onChange={function (): void {
-                  throw new Error("Function not implemented.");
-                }}
-              />
-            )}
+          <div className={style.lineTitle}>
+            <h4>Fonte: </h4>
+          </div>
+          {type === "Renda" ? (
+            <GroupIncome
+              onChange={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          ) : (
+            <GroupExpanses
+              onChange={function (): void {
+                throw new Error("Function not implemented.");
+              }}
+            />
+          )}
         </form>
       </div>
       <div className={style.btn_container}>
